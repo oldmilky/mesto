23bd69a1bef5d7974379be6be0178e1ae9244453
Rodion Strelkov <oldmilky@yandex.kz>
2021-03-17 03:08:57 +0600

    изменения после ревью #1

--- scripts/index.js
+++ scripts/index.js
@@ -8,32 +8,31 @@ let popupForm = popup.querySelector('.popup__form');
 let popupName = document.querySelector('.popup__input_name_name');
 let popupJob = document.querySelector('.popup__input_name_profession');
 
-// Событие открытия попапа
-function openPopup() {
-    popup.classList.add('popup_opened');
+// Событие открытия попапа 
+function openPopup() { 
+    popup.classList.add('popup_opened'); 
+} 
+// Событие зыкрытия попапа 
+function closePopup() { 
+    popup.classList.remove('popup_opened'); 
+} 
+//Обработчик открытия 
+popupButton.addEventListener('click', openPopup);
+// Функция для изначального заполнения значений полей формы
+let fillValueForm = () => { 
+    popupName.value = profileName.textContent;
+    popupJob.value = profileJob.textContent;
+    openPopup(); 
 }
-// Событие зыкрытия попапа
-function closePopup() {
-    popup.classList.remove('popup_opened');
-}
-//Обработчик открытия
-popupButton.addEventListener('click', function(event) {
-    openPopup();
-});
-//Закрытия попапа по клике на крестик
-popupButtonClose.addEventListener('click', closePopup);
-// Событие закрытия попапа на что угодно, кроме самого попапа (С моей версткой, другого способа не придумал)
-window.onclick = function(event) {
-    if(event.target == popup) {
-        closePopup();
-    }
-}
-// Обработчик формы
-function formSubmitHandler(event) {
-    event.preventDefault();
-    profileName.textContent = popupName.value;
-    profileJob.textContent = popupJob.value;
-    closePopup();
-}
-// Кнопка сохранить, закрывающий попап
-popupForm.addEventListener('submit', formSubmitHandler);
+popupButton.addEventListener('click', fillValueForm);
+//Закрытия попапа по клике на крестик 
+popupButtonClose.addEventListener('click', closePopup); 
+// Обработчик формы 
+function formSubmitHandler(event) { 
+    event.preventDefault(); 
+    profileName.textContent = popupName.value; 
+    profileJob.textContent = popupJob.value; 
+    closePopup(); 
+} 
+// Кнопка сохранить, закрывающий попап 
+popupForm.addEventListener('submit', formSubmitHandler);
